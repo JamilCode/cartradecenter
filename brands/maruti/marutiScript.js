8c3c4111c667116ae502b087c55cf3ed3e4d5f62
Jamil Ahmed <52717246+JamilCode@users.noreply.github.com>
2024-03-10 20:37:21 +0530

    tata and maruti uploaded

---
+++ brands/maruti/marutiScript.js
@@ -0,0 +1,54 @@
+let brand = document.getElementById('model-name').textContent;
+let changeSlide;
+let mainSlide = document.getElementById("main-slide");
+let createSlide = '';
+const apiKey = 'pat4RGJI6bRore9PP.0beca5b9a027f3be1e5c841e937912f8bc7d447455e1de48936079dd53b3974e';
+const baseId = 'appsrKnt4Xd6lTYza';
+const apiUrl = `https://api.airtable.com/v0/${baseId}/${brand}`;
+
+fetch(apiUrl, {
+    headers: {
+        'Authorization': `Bearer ${apiKey}`
+    }
+})
+.then(response => response.json())
+.then(data => {
+	const sortedData = data.records.sort((a, b) => {
+        const nameA = a.fields['Image Name'].toLowerCase(); // Assuming 'Image Name' is the column name
+        const nameB = b.fields['Image Name'].toLowerCase();
+        return nameA.localeCompare(nameB);
+    });
+	
+    for(let i=0; i<data.records.length; i++){
+        createSlide += `<div class="slide fade">
+            <span class="serial-number">${i+1}/${data.records.length}</span>
+            <img src="${data.records[i].fields.Photos[0].url}" />
+            <h2 class="caption">${data.records[i].fields.Title}</h2>
+            </div>`;
+    };
+    mainSlide.innerHTML = createSlide + `<a class="prev" onclick="changeSlide(-1)">❮</a>
+        <a class="next" onclick="changeSlide(1)">❯</a>`;
+    
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    changeSlide = function plusSlides(n) {
+        showSlides(slideIndex += n);
+    };
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("slide");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+            for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        slides[slideIndex-1].style.display = "block";  
+    }
+})
+.catch(error => console.error('Error fetching data:', error));
